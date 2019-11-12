e5d0354635dd74537fa97775b4cdeb736a288921
mpigelati <saimohansai@gmail.com>
2019-11-12 20:08:23 +0530

    Learning

---
+++ 3_Terraform_VPC_variable definition/var.tf
@@ -0,0 +1,11 @@
+variable "Taccess_key" {
+  default = "AKIAVS4FZVJMP243Y4GC"
+}
+
+variable "Tsecret_key" {
+  default = "yUsYTqAMiZS1JyKSiwZ33RJ7oKl6uVdT4dzsMuIz"
+}
+
+variable "Tregion" {
+  default = "us-east-1"
+}
